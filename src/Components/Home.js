f4971ddc282ccaf51908b3a593bb70e010ffe08f
timothee keck <timotheek92160@gmail.com>
2025-02-14 09:43:01 +0100

    Initial commit

---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="banner">
+        <h1>RUNNING</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
